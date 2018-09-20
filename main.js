342ce67b7c60fc6aad1a1a306b1e0ef61a7f8e6b
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-09-20 18:04:04 +0000

    get data onto page

--- main.js
+++ main.js
@@ -1,31 +1,89 @@
-// json.parse section
-var xhr = new XMLHttpRequest();         //xml = extensible markup language
-var data;
+const baseURL = "https://swapi.co/api/";
 
-xhr.open("GET", "https://swapi.co/api/");
-
-xhr.send();
-
-
-xhr.onreadystatechange = function() {           //xhr object maintains an internal state as it completes various parts of the request operation
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
     
-    if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+    
+    xhr.onreadystatechange = function() {           //xhr object maintains an internal state as it completes various parts of the request operation
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+            
+        }
+    };
+}
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
+
+// json.parse section
+/*
+const baseURL = "https://swapi.co/api/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();         //xml = extensible markup language
+    
+    //var data;
+    
+    xhr.onreadystatechange = function() {           //xhr object maintains an internal state as it completes various parts of the request operation
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+            
+        }
+    };
+    
+    xhr.open("GET", baseURL + type + "/");
+    
+    xhr.send();
+}
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
+ 
+*/   
+    
+    /*
         //data = this.responseText;
         //console.log(JSON.parse(this.responseText));
         //console.log(typeof(JSON.parse(this.responseText)));
         //console.log(typeof(this.responseText)); in inspect element this code showed a string in console
         
+    */
         
-    }
-};
 
 
+
+
+
+
+
+/*
+function printDataToConsole(data)  {
+    console.log(data);
+}
+
+getData(printDataToConsole);
+
+*/
+/*getData(function(data) {
+    console.log(data);
+});
+
+*/
+//set timeout function
+/*
 setTimeout(function() {
     console.log(data);
 }, 500);
 
-
+*/
 
 /*
 var xhr = new XMLHttpRequest();         //xml = extensible markup language
